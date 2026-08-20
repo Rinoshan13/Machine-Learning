c0af54b3ef8f9c2a76e5e2e0f0ffc5b2887c2b5e
Rinoshan Parameswaran <rparameswaran@invorg.com>
2026-08-21 00:44:21 +0200

    feat:add posterior log computation function

--- Spam-Classifier/Spam_classifier.py
+++ Spam-Classifier/Spam_classifier.py
@@ -121,4 +121,44 @@ plt.tight_layout()
 plt.savefig(HERE/"spam_ham_top_words.png", dpi=300, bbox_inches="tight")
 plt.show()
 
+
+# Classfication using MAP
+def compute_log_posterior(X,Theta,p_spam,p_ham):
+    log_theta_ham = np.log(Theta[:,0])
+    log_theta_spam = np.log(Theta[:,1])
+    log_1_theta_ham = np.log(1-Theta[:,0])
+    log_1_theta_spam = np.log(1-Theta[:,1])
+
+    # log P(x | ham) = sum_i [ x_i*log(theta_h_i) + (1-x_i)*log(1-theta_h_i) ]
+    log_p_x_ham = X @ log_theta_ham + (1-X) @log_1_theta_ham
+    log_p_x_spam = X @ log_theta_spam + (1-X) @ log_1_theta_spam
+
+    log_post_ham = log_p_x_ham +np.log(p_ham)
+    log_post_spam = log_p_x_spam +np.log(p_spam)
+
+    # P(spam|x) via log-diff trick
+    log_diff = log_post_spam - log_post_ham
+    p_spam_given_x = 1 / (1 + np.exp(-log_diff))
+    return p_spam_given_x
+
+probs_test = compute_log_posterior(X_test, Theta, p_spam, p_ham)
+preds_test = probs_test >= 0.5
+
+TP = np.sum((preds_test == True) & (Y_test == 'spam'))
+FN = np.sum((preds_test == False) & (Y_test == 'spam'))
+FP = np.sum((preds_test == True) & (Y_test == 'ham'))
+TN = np.sum((preds_test == False) & (Y_test == 'ham'))
+
+TPR = TP / (TP + FN)  
+FPR = FP / (FP + TN)   
+accuracy = (TP + TN) / len(Y_test)
+
+print(f"True Positive Rate (Sensitivity): {TPR:.4f}")
+print(f"False Positive Rate (1-Specificity): {FPR:.4f}")
+print(f"Accuracy: {accuracy:.4f}")
+print(f"\nConfusion Matrix:")
+print(f"  TP={TP}, FN={FN}")
+print(f"  FP={FP}, TN={TN}")
+
+
      
